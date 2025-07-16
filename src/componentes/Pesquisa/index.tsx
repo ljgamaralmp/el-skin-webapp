f223cc583dd788f0e3b3efc126e83c630836843d
Lucas Amaral <ljgamaral@mppr.mp.br>
2025-07-16 13:08:37 -0300

    Atualização arquivo de Pesquisa

--- src/componentes/Pesquisa/index.tsx
+++ src/componentes/Pesquisa/index.tsx
@@ -3,8 +3,6 @@ import lupa from '../../assets/lupa.svg'
 import styled from 'styled-components'
 import { useSearch } from '../../contexts/SearchContext'
 
-
-
 const PesquisaContainer = styled.div`
     background-color: white;
     position: relative;
@@ -24,15 +22,9 @@ const IconeLupa = styled.img`
     height: 25px;
     cursor: pointer;
 `
-
-
-
 export default function Pesquisa() {
-    // 2. Pega o estado e a função de atualização diretamente do contexto
     const { termoDeBusca, setTermoDeBusca } = useSearch();
     return (
-
-
             <PesquisaContainer>
                 <Input value={termoDeBusca} onChange={(evento) => setTermoDeBusca(evento.target.value)}
                 />
