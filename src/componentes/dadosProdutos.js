8d1336e3af86462b2be39dc0b5dc37cac19da329
Lucas Amaral <ljgamaral@mppr.mp.br>
2025-07-17 15:06:18 -0300

    Execução json-server e instalação biblioteca Axios

---
+++ src/componentes/dadosProdutos.js
@@ -0,0 +1,10 @@
+import produtoImg from '../assets/produto.png'; // Supondo que você tem uma imagem
+
+const dadosProdutos = [
+  { id: 1, nome: 'Protetor Solar AL SUN', descricao: 'alta proteção e pele luminosa.', preco: 'R$ 79,90', imagem: produtoImg },
+  { id: 2, nome: 'Sérum Hidratante Aqua', descricao: 'hidratação profunda com ácido hialurônico.', preco: 'R$ 89,90', imagem: produtoImg },
+  { id: 3, nome: 'Creme Noturno Revitalizante', descricao: 'repara a pele enquanto você dorme.', preco: 'R$ 99,90', imagem: produtoImg },
+  { id: 4, nome: 'Limpeza Facial Pureza', descricao: 'remove impurezas sem ressecar a pele.', preco: 'R$ 59,90', imagem: produtoImg }
+];
+
+export default dadosProdutos;
